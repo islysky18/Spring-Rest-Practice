f59cf957c08fe64122723ad745259a80aa00527b
TobiasWen <islysky18@gmail.com>
2020-10-30 01:32:53 -0700

    Using @PathVariable to get a single student in the list

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +16,31 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
-	// define enpoint for "/students" -> return list of student
+	private List<Student> theStudents;
+	
+	//define @PostCinstruct to load the student data.. only once!
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>();
+		theStudents.add(new Student("Chao-Ting", "Wen"));
+		theStudents.add(new Student("Siqi", "Geng"));
+		theStudents.add(new Student("John", "Wick"));	
+	}
+	
+	// define endpoint for "/students" -> return list of student
 	
 	@GetMapping("/students")
 	public List<Student> getStudents() {
 		
-		List<Student> theStudents = new ArrayList<Student>();
-		theStudents.add(new Student("Chao-Ting", "Wen"));
-		theStudents.add(new Student("Siqi", "Geng"));
-		theStudents.add(new Student("John", "Wick"));
 		return theStudents;
+	}
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		
+		//just index into the list
+		
+		return theStudents.get(studentId);
 	}
 	
 }
